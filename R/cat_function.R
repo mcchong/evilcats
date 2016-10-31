045d677f9cf9af4585a91c987433a46e518ba6c6
Mike Chong <mcchong@uwaterloo.ca>
2016-10-31 14:08:15 -0400

    created basic function

---
+++ R/cat_function.R
@@ -0,0 +1,17 @@
+# From: https://hilaryparker.com/2014/04/29/writing-an-r-package-from-scratch/
+#' A Cat Function
+#'
+#' This function allows you to express your love of cats.
+#' @param love Do you love cats? Defaults to TRUE.
+#' @keywords cats
+#' @export
+#' @examples
+#' cat_function()
+cat_function <- function(love=TRUE){
+    if(love==TRUE){
+        print("I love cats!")
+    }
+    else {
+        print("I am not a cool person.")
+    }
+}
